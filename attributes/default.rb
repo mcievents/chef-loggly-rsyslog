f024bf4f247406d8ab594e7d13fb755bf46bcfed
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2018-04-10 13:56:23 -0500

    update checksum for loggly certificate

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['loggly']['log_files'] = []
 default['loggly']['tls']['enabled'] = true
 default['loggly']['tls']['cert_path'] = '/etc/rsyslog.d/keys/ca.d'
 default['loggly']['tls']['cert_url'] = 'https://logdog.loggly.com/media/logs-01.loggly.com_sha12.crt'
-default['loggly']['tls']['cert_checksum'] = 'b562ae82b54bcb43923290e78949153c0c64910d40b02d2207010bb119147ffc'
+default['loggly']['tls']['cert_checksum'] = '3bcd557ea8e43599d3fb98fd13857c2fbff5637cb307a3d4bae23e3c8b5c34cc'
 
 default['loggly']['rsyslog']['host'] = 'logs-01.loggly.com'
 default['loggly']['rsyslog']['port'] = node['loggly']['tls']['enabled'] ? 6514 : 514
